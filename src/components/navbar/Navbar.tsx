79a0ddffab94e626dc384089bd74a279f8f1fb77
KaiserPhemi <kaiser.phemi@protonmail.ch>
2022-12-27 00:58:13 +0100

    chore()
    - install dependencies

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+// styles
+import "./navbar.css";
+
+// navbar component
+const Navbar = () => {
+  return (
+    <div className="countries__navbar">
+      <div>
+        <p>Where in the world?</p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
