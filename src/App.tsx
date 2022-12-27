99579398ddedf3ddc52f445439f9e5603c1c4414
KaiserPhemi <kaiser.phemi@protonmail.ch>
2022-12-27 01:21:32 +0100

    chore()
    - make API calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+// styles
 import "./App.css";
 
+// api
+import { fetchCountries } from "./api";
+
+// main app component
 const App = () => {
-  return <div className="App"></div>;
+  /**
+   * Fetch countries
+   */
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetchCountries.fetchAll();
+      console.log("the result", result.data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <p>We got here</p>
+    </div>
+  );
 };
 
 export default App;
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,15 @@
-export const fetchCountries = {}
+// third-party libraries
+import axios from 'axios';
+
+// constant
+const BASE_URL: string = process.env.REACT_APP_COUNTRIES_API!;
+
+// Methods to make API calls
+export const fetchCountries = {
+  /**
+   * Fetches all countries
+   */
+  async fetchAll() {
+    return await axios.get(`${BASE_URL}/all`)
+  }
+} 
