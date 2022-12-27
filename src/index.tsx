a94a9db2ddbafe507462a662b20490618d655a63
KaiserPhemi <kaiser.phemi@protonmail.ch>
2022-12-27 20:49:41 +0100

    chore()
    - optimising app

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// react libraries
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// styles
+import "./index.css";
+
+// component(s)
+import App from "./App";
+
+// tools
+import reportWebVitals from "./reportWebVitals";
+import { register } from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
@@ -17,3 +25,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+register();
