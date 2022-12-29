096bec24660aaf88ea801486b5c6101e40fe9e2f
KaiserPhemi <kaiser.phemi@protonmail.ch>
2022-12-29 22:13:40 +0100

    chore() - add new image assets

--- src/components/countryDetail/CountryDetail.tsx
+++ src/components/countryDetail/CountryDetail.tsx
@@ -9,6 +9,7 @@ import styles from "./country.module.css";
 
 // country detail component
 const CountryDetail = ({ country }: any) => {
+  console.log("inside", country);
   return (
     <div className={styles.country_detail}>
       <Link to="/">
@@ -19,7 +20,38 @@ const CountryDetail = ({ country }: any) => {
       </Link>
       <section>
         <section></section>
-        <section></section>
+        <section>
+          <h2>{country.name.common}</h2>
+          <section>
+            <dl>
+              <dt>Native Name</dt>
+              <dd></dd>
+              <dt>Population</dt>
+              <dd></dd>
+              <dt>Region</dt>
+              <dd></dd>
+              <dt>Sub Region</dt>
+              <dd></dd>
+              <dt>Capital</dt>
+              <dd>{country.capital[0]}</dd>
+              <dt>Top Level Domain</dt>
+              <dd>{country.tld[0]}</dd>
+              <dt>Currency</dt>
+              <dd>
+                {country.currencies[Object.keys(country.currencies)[0]].name}
+              </dd>
+              <dt>Languages</dt>
+              <dd>{country.languages[Object.keys(country.languages)[0]]}</dd>
+              <dt></dt>
+              <dd></dd>
+            </dl>
+          </section>
+          <section>
+            Border Countries:
+            {}
+            <button></button>
+          </section>
+        </section>
       </section>
     </div>
   );
