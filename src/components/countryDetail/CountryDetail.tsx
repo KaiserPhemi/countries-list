3de67e0eba3525ac453beb5d8f68aaa9a12b43cb
KaiserPhemi <kaiser.phemi@protonmail.ch>
2022-12-30 11:38:22 +0100

    feature()
    - define global color variables

--- src/components/countryDetail/CountryDetail.tsx
+++ src/components/countryDetail/CountryDetail.tsx
@@ -5,11 +5,12 @@ import { Link } from "react-router-dom";
 // third party libraries
 import { FiArrowLeft } from "react-icons/fi";
 // styles
-import styles from "./country.module.css";
+import styles from "./countryDetail.module.css";
 
 // country detail component
 const CountryDetail = ({ country }: any) => {
   console.log("inside", country);
+  const nativeName: { [key: string]: any } = country.name.nativeName;
 
   return (
     <div className={styles.country_detail}>
@@ -25,36 +26,37 @@ const CountryDetail = ({ country }: any) => {
         </section>
         <section className={styles.details_section_data}>
           <h2>{country.name.common}</h2>
-          <section className="">
-            <section className="">
-              <dl>
-                <dt>Native Name</dt>
-                <dd></dd>
-                <dt>Population</dt>
-                <dd>{country.population.toLocaleString("en-US")}</dd>
-                <dt>Region</dt>
-                <dd>{country.region}</dd>
-                <dt>Sub Region</dt>
-                <dd></dd>
-                <dt>Capital</dt>
-                <dd>{country.capital[0] || country.capital}</dd>
-              </dl>
-            </section>
-            <section className="">
-              <dl>
-                <dt>Top Level Domain</dt>
-                <dd>{country.tld[0]}</dd>
-                <dt>Currency</dt>
-                <dd></dd>
-                <dt>Languages</dt>
-                <dd></dd>
-              </dl>
-            </section>
+          <section className={styles.country_data}>
+            <dl>
+              <dt>Native Name: </dt>
+              <dd>{nativeName[Object.keys(nativeName)[0]].official}</dd>
+              <dt>Population: </dt>
+              <dd>{country.population.toLocaleString("en-US")}</dd>
+              <dt>Region: </dt>
+              <dd>{country.region}</dd>
+              <dt>Sub Region</dt>
+              <dd>{country.subregion}</dd>
+              <dt>Capital: </dt>
+              <dd>{country.capital[0] || country.capital}</dd>
+            </dl>
+            <dl>
+              <dt>{"Top Level Domain: "}</dt>
+              <dd>{country.tld[0]}</dd>
+              <dt>{"Currency: "}</dt>
+              <dd>
+                {country.currencies[Object.keys(country.currencies)[0]].name}
+              </dd>
+              <dt>{"Currency Symbol: "}</dt>
+              <dd>
+                {country.currencies[Object.keys(country.currencies)[0]].symbol}
+              </dd>
+              <dt>Languages: </dt>
+              {Object.values(country.languages).join(", ")}
+            </dl>
           </section>
-          <section>
-            Border Countries:
-            {}
-            <button></button>
+          <section className={styles.border_countries_list}>
+            <p>{`Border Countries: `}</p>
+            {<button className={styles.border_countries_item}>Nigeria</button>}
           </section>
         </section>
       </section>
